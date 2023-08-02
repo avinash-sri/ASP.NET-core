66368bb65f765a5f28a7cf7054f2eb708793e8b5
Avinash <avinashsrivastav76@gmail.com>
2023-08-02 12:38:57 +0530

    added mvc

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -5,11 +5,47 @@ namespace BookStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+
+
+            /* Middlewares
+             app.Use(async (context,next) =>
+            {
+                await context.Response.WriteAsync("Hello from my first middleware ");
+                await next.Invoke();
+
+                await context.Response.WriteAsync("Hello from my first middleware response ");
+
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from my second middleware ");
+                await next.Invoke();
+
+                await context.Response.WriteAsync("Hello from my second middleware response ");
+
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello from my third middleware");
+            });*/
+
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.MapDefaultControllerRoute();
 
             app.Run();
+
+
         }
     }
 }
